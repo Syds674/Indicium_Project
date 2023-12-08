320ba0d6590d2d5af9a0211ef355ca2223b107fd
Ytalo-Douglas-ss <121064190+Syds674@users.noreply.github.com>
2023-12-08 08:55:22 -0300

    Update Process_ETL.py

--- indicium/scripts/Process_ETL.py
+++ indicium/scripts/Process_ETL.py
@@ -4,11 +4,19 @@ from datetime import datetime
 # Obtem a data atual no formato YYYY-MM-DD
 data_atual = datetime.now().strftime('%Y-%m-%d')
 
-# Caminho do script spark-submit
-spark_script = "/indicium/scripts/extracao_dados_order_details.py"
+# Caminho dos scripts spark-submit
+script1 = "/indicium/scripts/extracao_dados_order_details.py"
+script2 = "/indicium/scripts/process_order_details.py"
+script3 = "/indicium/scripts/extracao_dados_orders.py"
+script4 = "/indicium/scripts/process_orders.py"
 
 # Lista de scripts a serem executados na ordem desejada
-scripts_to_run = [f"spark-submit {spark_script} {data_atual}"]
+scripts_to_run = [
+    f"spark-submit {script1} {data_atual}",
+    f"spark-submit {script2} {data_atual}",
+    f"spark-submit {script3} {data_atual}",
+    f"spark-submit {script4} {data_atual}"
+]
 
 for script in scripts_to_run:
     try:
